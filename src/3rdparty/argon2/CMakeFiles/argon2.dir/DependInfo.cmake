f5738badb1dc804a5146d671a1e9d210c53fbb87
maribun20 <maribun23@gmail.com>
2021-03-06 02:16:44 +0700

    upload bin cmake doc res scripts src

---
+++ src/3rdparty/argon2/CMakeFiles/argon2.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/xmrig-6.9.0/src/3rdparty/argon2/arch/generic/lib/argon2-arch.c" "/root/xmrig-6.9.0/src/3rdparty/argon2/CMakeFiles/argon2.dir/arch/generic/lib/argon2-arch.c.o"
+  "/root/xmrig-6.9.0/src/3rdparty/argon2/lib/argon2.c" "/root/xmrig-6.9.0/src/3rdparty/argon2/CMakeFiles/argon2.dir/lib/argon2.c.o"
+  "/root/xmrig-6.9.0/src/3rdparty/argon2/lib/blake2/blake2.c" "/root/xmrig-6.9.0/src/3rdparty/argon2/CMakeFiles/argon2.dir/lib/blake2/blake2.c.o"
+  "/root/xmrig-6.9.0/src/3rdparty/argon2/lib/core.c" "/root/xmrig-6.9.0/src/3rdparty/argon2/CMakeFiles/argon2.dir/lib/core.c.o"
+  "/root/xmrig-6.9.0/src/3rdparty/argon2/lib/encoding.c" "/root/xmrig-6.9.0/src/3rdparty/argon2/CMakeFiles/argon2.dir/lib/encoding.c.o"
+  "/root/xmrig-6.9.0/src/3rdparty/argon2/lib/genkat.c" "/root/xmrig-6.9.0/src/3rdparty/argon2/CMakeFiles/argon2.dir/lib/genkat.c.o"
+  "/root/xmrig-6.9.0/src/3rdparty/argon2/lib/impl-select.c" "/root/xmrig-6.9.0/src/3rdparty/argon2/CMakeFiles/argon2.dir/lib/impl-select.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CL_TARGET_OPENCL_VERSION=200"
+  "CL_USE_DEPRECATED_OPENCL_1_2_APIS"
+  "HAVE_BUILTIN_CLEAR_CACHE"
+  "HAVE_SYSLOG_H"
+  "NDEBUG"
+  "UNICODE"
+  "XMRIG_ALGO_ARGON2"
+  "XMRIG_ALGO_RANDOMX"
+  "XMRIG_ARM"
+  "XMRIG_ARM_CRYPTO"
+  "XMRIG_ARMv8"
+  "XMRIG_FEATURE_ADL"
+  "XMRIG_FEATURE_API"
+  "XMRIG_FEATURE_BENCHMARK"
+  "XMRIG_FEATURE_CUDA"
+  "XMRIG_FEATURE_ENV"
+  "XMRIG_FEATURE_HTTP"
+  "XMRIG_FEATURE_HWLOC"
+  "XMRIG_FEATURE_NVML"
+  "XMRIG_FEATURE_OPENCL"
+  "XMRIG_JSON_SINGLE_LINE_ARRAY"
+  "XMRIG_MINER_PROJECT"
+  "XMRIG_OS_LINUX"
+  "XMRIG_OS_UNIX"
+  "XMRIG_STRICT_OPENCL_CACHE"
+  "_FILE_OFFSET_BITS=64"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src/3rdparty/argon2/../.."
+  "src/3rdparty/argon2/lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
